1bd5bbcb2c961fd6d4f655b1bc3f52bafd2558c1
alyfazeha <alyfazeha@gmail.com>
2026-02-25 16:15:50 +0700

    Adding Practicum (Study Case Folder)

---
+++ Week2/study_case_week2/bin/study_case_week2.dart
@@ -0,0 +1,5 @@
+import 'package:study_case_week2/study_case_week2.dart' as study_case_week2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${study_case_week2.calculate()}!');
+}
---
+++ Week2/study_case_week2/test/study_case_week2_test.dart
@@ -0,0 +1,8 @@
+import 'package:study_case_week2/study_case_week2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
