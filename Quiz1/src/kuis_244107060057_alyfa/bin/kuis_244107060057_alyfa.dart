d7b0274189c608af933eacf7845af18d50933d48
alyfazeha <alyfazeha@gmail.com>
2026-03-11 13:15:21 +0700

    Setup project structure for Quiz1 [244107060057]

---
+++ Quiz1/src/kuis_244107060057_alyfa/bin/kuis_244107060057_alyfa.dart
@@ -0,0 +1,5 @@
+import 'package:kuis_244107060057_alyfa/kuis_244107060057_alyfa.dart' as kuis_244107060057_alyfa;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kuis_244107060057_alyfa.calculate()}!');
+}
---
+++ Quiz1/src/kuis_244107060057_alyfa/test/kuis_244107060057_alyfa_test.dart
@@ -0,0 +1,8 @@
+import 'package:kuis_244107060057_alyfa/kuis_244107060057_alyfa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
