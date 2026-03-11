7243bb8ce3d970ebc0ae7dc60f3475af51d4dc02
alyfazeha <alyfazeha@gmail.com>
2026-03-11 13:27:31 +0700

    Adding variable and data type to store things price [244107060057]

--- Quiz1/src/kuis_244107060057_alyfa/bin/kuis_244107060057_alyfa.dart
+++ Quiz1/src/kuis_244107060057_alyfa/bin/kuis_244107060057_alyfa.dart
@@ -5,4 +5,7 @@ void main(List<String> arguments) {
   String nama = "Alyfa Zahra Qurrota Aini";
   String nim = "244107060057";
   double nilaiUnikNIM = 057;
+
+  List<double> hargaBarang = [55000, 35500, 15000, 60000, 7500];
+  hargaBarang.add(nilaiUnikNIM);
 }
