b503eefe981d4e67ead27e4c35fd0133e08808cc
alyfazeha <alyfazeha@gmail.com>
2026-03-11 13:23:29 +0700

    Declaration variable Nama, NIM, and nilaiUnikNIM [244107060057]

--- Quiz1/src/kuis_244107060057_alyfa/bin/kuis_244107060057_alyfa.dart
+++ Quiz1/src/kuis_244107060057_alyfa/bin/kuis_244107060057_alyfa.dart
@@ -1,5 +1,8 @@
-import 'package:kuis_244107060057_alyfa/kuis_244107060057_alyfa.dart' as kuis_244107060057_alyfa;
+import 'package:kuis_244107060057_alyfa/kuis_244107060057_alyfa.dart'
+    as kuis_244107060057_alyfa;
 
 void main(List<String> arguments) {
-  print('Hello world: ${kuis_244107060057_alyfa.calculate()}!');
+  String nama = "Alyfa Zahra Qurrota Aini";
+  String nim = "244107060057";
+  double nilaiUnikNIM = 057;
 }
